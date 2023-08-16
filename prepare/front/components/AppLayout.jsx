a8ed13c5137548a35568ae4ccdf3e250da33bf0f
cham0287 <crtmt97@gmail.com>
2023-08-17 07:35:41 +0900

    Feat: Layout Link 적용

--- prepare/front/components/AppLayout.jsx
+++ prepare/front/components/AppLayout.jsx
@@ -1,8 +1,13 @@
+import { Link } from 'next/link';
 import PropTypes from 'prop-types';
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
+      <div>
+        <Link>노드버드</Link>
+        <Link>프로필</Link>
+        <Link>회원가입</Link>
+      </div>
       {children}
     </div>
   );
